8bd301ad14892beed88ff94e606356e05f7c1b22
Joaquín Nicolás Montero Esteves <joaquinnme@outlook.com>
2023-08-23 17:20:52 -0300

    Update mock.router.js

--- src/Routes/mock.router.js
+++ src/Routes/mock.router.js
@@ -1,11 +1,11 @@
-import { Router } from "express"
-import { generateProducts } from "../services/mock/products.mock.js"
+//import { Router } from "express"
+//import { generateProducts } from "../services/mock/products.mock.js"
 
-const routerMocks = new Router()
+//const routerMocks = new Router()
 
-routerMocks.get('/', (req, res) => {
-  const mockProducts = generateProducts()
-  res.status(201).json( { mockProducts })
-})
+//routerMocks.get('/', (req, res) => {
+  //const mockProducts = generateProducts()
+  //res.status(201).json( { mockProducts })
+//})
 
-export default routerMocks
+//export default routerMocks
